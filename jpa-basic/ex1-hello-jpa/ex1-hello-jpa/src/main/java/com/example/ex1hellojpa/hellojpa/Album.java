2081e1060156b6cab2009e86d5960c0742a68ee1
qwerty1434 <qwerty1434@naver.com>
2022-11-22 22:47:25 +0900

    1122

--- jpa-basic/ex1-hello-jpa/ex1-hello-jpa/src/main/java/com/example/ex1hellojpa/hellojpa/Album.java
+++ jpa-basic/ex1-hello-jpa/ex1-hello-jpa/src/main/java/com/example/ex1hellojpa/hellojpa/Album.java
@@ -4,5 +4,5 @@ import javax.persistence.Entity;
 
 @Entity
 public class Album extends Item{
-    private String artiest;
+    private String artist;
 }
--- jpa-basic/ex1-hello-jpa/ex1-hello-jpa/src/main/java/com/example/ex1hellojpa/hellojpa/Item.java
+++ jpa-basic/ex1-hello-jpa/ex1-hello-jpa/src/main/java/com/example/ex1hellojpa/hellojpa/Item.java
@@ -1,13 +1,48 @@
 package com.example.ex1hellojpa.hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
-public class Item {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 하나의 테이블에 모든 정보를 받게
+@DiscriminatorColumn // DTYPE 활용
+public abstract class Item { // 추상 클래스
     @Id @GeneratedValue
+    @Column(name="ITEM_ID")
     private Long id;
+
     private String name;
     private int price;
+    private int stockQuantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
 }

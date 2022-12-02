993540667fdd196db8dcca6c299148ac6d737a87
qwerty1434 <qwerty1434@naver.com>
2022-12-02 19:45:59 +0900

    1202

--- jpa-util1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpa-util1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,5 +1,10 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -7,6 +12,9 @@ import java.util.List;
 
 @Entity
 @Table(name="orders") // mysql에 예약어 order(order by 할 때 order)가 있기 때문에 이름을 변경해 줍니다.
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
     @Id @GeneratedValue
@@ -29,6 +37,7 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태
 
+    //==연관관계 메서드==//
     public void setMember(Member member){
         this.member = member;
         member.getOrders().add(this);
@@ -41,4 +50,44 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    //==생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){ // OrderItem여러개를 리스트로 넘긴다는 걸 ...으로 표현함
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for(OrderItem orderItem : orderItems){
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+
+    //==비즈니스 로직==// 비즈니스 로직을 Entity안에 넣었다는 게 특징입니다.(이게 편리하다고 합니다)
+    /*
+     * 주문 취소
+     */
+    public void cancel(){
+        if(delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem orderItem : orderItems){
+            orderItem.cancel(); // 주문이 취소되면 order의 수량도 모두 되돌려야 합니다.
+        }
+    }
+    /*
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice(){
+        int totalPrice = 0;
+        for(OrderItem orderItem : orderItems){
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
 }
--- jpa-util1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpa-util1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -25,5 +25,28 @@ public class OrderItem {
     private int orderPrice; // 주문 가격
     private int count; // 주문 수량
 
+    protected OrderItem(){} // 밑에 create메서드를 따로 만들었기 때문에 생성자를 통한 생성을 막아둡시다.
 
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회로직==/
+    /*
+     * 주문상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }

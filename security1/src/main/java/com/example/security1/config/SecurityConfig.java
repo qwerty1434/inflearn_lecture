4f4f238ef8b890b6c6487b620f5e7535c5e940aa
qwerty1434 <qwerty1434@naver.com>
2023-01-06 17:07:05 +0900

    0106

--- security1/src/main/java/com/example/security1/config/SecurityConfig.java
+++ security1/src/main/java/com/example/security1/config/SecurityConfig.java
@@ -31,6 +31,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/loginForm")
                 .loginProcessingUrl("/login") // login주소가 호출되면 시큐리티가 낚아채서 대신 로그인을 진행해 줍니다.
-                .defaultSuccessUrl("/");
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .loginPage("/loginForm");
     }
 }

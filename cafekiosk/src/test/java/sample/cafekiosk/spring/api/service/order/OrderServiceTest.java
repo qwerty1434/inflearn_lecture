92f53c51c0089fa3b3c6ba2615fc8730e2420563
qwerty1434 <qwerty1434@naver.com>
2023-07-10 21:30:46 +0900

    0710

--- cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -166,8 +166,9 @@ class OrderServiceTest {
         Product product3 = createProduct(HANDMADE,"003",5000);
         productRepository.saveAll(List.of(product1, product2, product3));
 
-        Stock stock1 = Stock.create("001",1);
-        Stock stock2 = Stock.create("002",1);
+        Stock stock1 = Stock.create("001",2);
+        Stock stock2 = Stock.create("002",2);
+        stock1.deductQuantity(1);
         stockRepository.saveAll(List.of(stock1,stock2));
 
         OrderCreateServiceRequest request = OrderCreateServiceRequest.builder()

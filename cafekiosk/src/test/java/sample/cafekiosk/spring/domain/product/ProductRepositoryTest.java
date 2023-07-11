65f274a022c60d6b762d90a0902e2e002ca56023
qwerty1434 <qwerty1434@naver.com>
2023-07-11 21:43:28 +0900

    0711

--- cafekiosk/src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -119,6 +119,7 @@ class ProductRepositoryTest {
         // then
         assertThat(latestProductNumber).isEqualTo(targetProductNumber);
     }
+
     @DisplayName("가장 마지막으로 저장한 상품의 상품번호를 읽어올 때, 상품이 하나도 없는 경우에는 null을 반환한다.")
     @Test
     void findLatestProductNumberWhenProductIsEmpty(){
@@ -128,7 +129,6 @@ class ProductRepositoryTest {
         // then
         assertThat(latestProductNumber).isNull();
     }
-
 
     private Product createProduct(String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
         return Product.builder()
